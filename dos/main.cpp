1b3ef3c8a08f24fb1fac0735d144c8180107bebb
Alexander Bladh <alexander@Alexanders-MacBook-Air.local>
2013-09-18 10:50:40 +0200

    Initial Commit

---
+++ dos/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  dos
+//
+//  Created by Alexander Bladh on 2013-09-18.
+//  Copyright (c) 2013 ab. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
